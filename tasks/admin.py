51694cfc0f877a5bd1e70ccffa36063b5056aece
Rumple <rumplestinki10@gmail.com>
2023-10-07 18:28:10 -0600

    Se creó el model Task y se agrego al panel de Admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Task
+
+# Indica que created es de solo lectura
+class TaskAdmin(admin.ModelAdmin):
+    readonly_fields = ("created", )
 
 # Register your models here.
+
+admin.site.register(Task, TaskAdmin)
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now=True)
+    datecomplete = models.DateTimeField(null=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    # Muestra en el panel de admin el title y el usuario
+    def __str__(self):
+        return self.title + ' - by ' + self.user.username
